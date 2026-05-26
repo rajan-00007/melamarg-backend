bff3adf27f6b1a93903655c74196fcb9ca513ae6
rajan-00007 <rajan.verma@healthligence.com>
2026-05-26 15:56:18 +0530

    style: add a blank line for improved readability in server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import logger from './utils/logger';
 
 const PORT = process.env.PORT || 5000;
 const HOST = '0.0.0.0';
- 
+  
 app.listen(Number(PORT), HOST, () => {
   logger.info(`Server is running on http://${HOST}:${PORT}`);
 });
