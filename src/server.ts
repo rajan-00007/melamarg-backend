65e48b5bb140e16c2cab02ce2dbb38203c705283
rajan-00007 <rajan.verma@healthligence.com>
2026-05-26 16:51:32 +0530

    new

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import logger from './utils/logger';
 
 const PORT = process.env.PORT || 5000;
 const HOST = '0.0.0.0';
-  
 app.listen(Number(PORT), HOST, () => {
   logger.info(`Server is running on http://${HOST}:${PORT}`);
 });
